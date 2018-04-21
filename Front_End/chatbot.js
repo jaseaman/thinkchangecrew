ad6daeda8e3f5b3e4d44f8b202b6df8ecf57519b
ggeroulas <georgy1@tpg.com.au>
2018-04-22 00:09:44 +1000

    Fixed chat bubble problem
    noot noot

--- Front_End/chatbot.js
+++ Front_End/chatbot.js
@@ -26,12 +26,20 @@ $(function () {
     })
 
     
-    $(".chat-input__text").keypress(function(event) { // Send message to chat bot
+    $(".chat-input__text").keypress(function(event) { // Send message to the chat bot
         //  &&  document.getElementById(".chat-input__text").value != "" use for null
         if (event.which == 13) {
-            
-            console.log("enter pressed, checkbox is checked");
+            // Send the request 
             event.preventDefault();
+            var userMessage = $(".chat-input__text").val();
+
+            var prevState =  $(".chat-logs").html();
+
+            
+            $(".chat-logs").html(prevState + "<br>" + "<div class=\"cm-msg-text\">" + userMessage + "</div>");
+
+            
+            
             $(".chat-input__text").val("");
 
         } 
