c2af402db0aa78e2ea84cd9437e5b25a60d22ed7
LAPTOP\Myhijim <myhijim@gmail.com>
2018-04-21 19:42:00 +1000

    Created IBookingService abstraction

---
+++ STU.Shared/Services/IBookingService.cs
@@ -0,0 +1,16 @@
+﻿using STU.Common.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STU.Shared.Services
+{
+    public interface IBookingService
+    {
+        Result BookRoom(string roomId, DateTime time);
+        Result CancelBooking(string bookingId);
+        Result<List<string>> CancelBooking(string roomId, DateTime time);
+    }
+}
