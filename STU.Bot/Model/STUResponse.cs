464930e47881b04840dea50da3878649d448ddc4
LAPTOP\Myhijim <myhijim@gmail.com>
2018-04-21 23:59:54 +1000

    Added STU Response base model

---
+++ STU.Bot/Model/STUResponse.cs
@@ -0,0 +1,13 @@
+﻿using STU.Shared.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace STU.Bot.Model
+{
+    public class STUResponse : BaseModel
+    {
+        List<string> Responses { get; set; }
+    }
+}
