02bd1a78fbd785bb401c65333add593f3f73c623
LAPTOP\Myhijim <myhijim@gmail.com>
2018-04-21 14:32:59 +1000

    Removed redundant system message if switches

--- STU.Bot/Controllers/MessagesController.cs
+++ STU.Bot/Controllers/MessagesController.cs
@@ -7,7 +7,6 @@ using Microsoft.Bot.Connector;
 
 namespace STU.Bot
 {
-    [BotAuthentication]
     public class MessagesController : ApiController
     {
         /// <summary>
@@ -30,29 +29,6 @@ namespace STU.Bot
 
         private Activity HandleSystemMessage(Activity message)
         {
-            if (message.Type == ActivityTypes.DeleteUserData)
-            {
-                // Implement user deletion here
-                // If we handle user deletion, return a real message
-            }
-            else if (message.Type == ActivityTypes.ConversationUpdate)
-            {
-                // Handle conversation state changes, like members being added and removed
-                // Use Activity.MembersAdded and Activity.MembersRemoved and Activity.Action for info
-                // Not available in all channels
-            }
-            else if (message.Type == ActivityTypes.ContactRelationUpdate)
-            {
-                // Handle add/remove from contact lists
-                // Activity.From + Activity.Action represent what happened
-            }
-            else if (message.Type == ActivityTypes.Typing)
-            {
-                // Handle knowing tha the user is typing
-            }
-            else if (message.Type == ActivityTypes.Ping)
-            {
-            }
 
             return null;
         }
