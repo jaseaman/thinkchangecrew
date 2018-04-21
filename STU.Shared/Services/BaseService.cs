6599c44c2507089bed77d01ac91cc9efe4a3cffd
LAPTOP\Myhijim <myhijim@gmail.com>
2018-04-22 02:48:54 +1000

    Added a BaseService abstract class

---
+++ STU.Shared/Services/BaseService.cs
@@ -0,0 +1,15 @@
+﻿using STU.Shared.Model;
+using STU.Shared.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STU.Shared.Services
+{
+    public abstract class BaseService<T> where T : BaseModel
+    {
+        public IRepository<T> Repository { get; set; }
+    }
+}
