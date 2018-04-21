463a88c22d0d1260b892c02be34260de53f7f345
Jason <32154076+JasonGilmore@users.noreply.github.com>
2018-04-22 03:16:28 +1000

    Revert "adds compatibility for images and string responses"
    This reverts commit c9ee7436b4f56b258f2c79429fd19da43d5d869a.

--- Front_End/chatbot.js
+++ Front_End/chatbot.js
@@ -30,33 +30,29 @@ $(function () {
     
     $(".chat-input__text").keypress(function(event) { // Send message to the chat bot
         //  &&  document.getElementById(".chat-input__text").value != "" use for null
-        if (event.which == 13) { 
+        if (event.which == 13) {
+            
             event.preventDefault();
             var userMessage = $(".chat-input__text").val();
-            var prevState =  $(".chat-logs").html();
-            $(".chat-logs").html(prevState + "<br>" + "<div class=\"chat-user\">" + userMessage + "</div>");
-            $(".chat-input__text").val("");
 
+            var prevState =  $(".chat-logs").html();
+
+            
+            $(".chat-logs").html(prevState + "<br>" + "<div class=\"chat-user\">" + userMessage + "</div>");
 
             // Send the request to server
             // Server returns the response, and is passed into robot message
             // Below the bot message passes in a placeholder
-            // Needs to input content and it's content type
+            
+
+        
+            $(".chat-input__text").val("");
 
             setTimeout(function() {
                 var prevState = $(".chat-logs").html();
-                var content = "Oi, I'm a placeholder";
-                //var content = "maxresdefault.jpg";
-                var contentType = "string"
-                if (contentType == "string") {
-                    $(".chat-logs").html(prevState + "<br>" + "<div><img class=\"msg-avatar\" src=\"STU icon.png\"/><div class=\"chat-bot\">" + content + "</div></div>");
+                var placeholder = "Oi, I'm a placeholder";
 
-                } else {
-                    $(".chat-logs").html(prevState + "<br>" + "<div><img class=\"msg-avatar\" src=\"STU icon.png\"/><div class=\"chat-bot\">" + "<image class=\"img\" src=\"" + content + "\"/>" + "</div></div>");
-
-                }
-                
-                //$(".chat-logs").html(prevState + "<br>" + "<div><img class=\"msg-avatar\" src=\"STU icon.png\"/><div class=\"chat-bot\">" + placeholder + "</div></div>");
+                $(".chat-logs").html(prevState + "<br>" + "<div><img class=\"msg-avatar\" src=\"STU icon.png\"/><div class=\"chat-bot\">" + placeholder + "</div></div>");
             }, 750);
            
 
