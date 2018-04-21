9d391809cbf410e4f9a690bcc41086481f36d93d
LAPTOP\Myhijim <myhijim@gmail.com>
2018-04-21 15:04:48 +1000

    Re-implemented the Bot Authentication

---
+++ STU.Bot/Dialogs/ResponseDialog.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace STU.Bot.Dialogs
+{
+    [Serializable]
+    public class ResponseDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task UserQueryRecievedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+
+        }
+    }
+}
